f4b1250191063cf18205a9f53bbaa012655c6771
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-07-18 00:24:43 +0330

    16

--- server/currency.go
+++ server/currency.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/mahdimehrabi/grpc-base-currency/data"
 	"github.com/mahdimehrabi/grpc-base-currency/proto/currency"
+	"io"
+	"time"
 )
 
 type CurrencyServer struct {
@@ -26,4 +28,29 @@ func (s CurrencyServer) GetRate(ctx context.Context, rr *currency.RateRequest) (
 	return &currency.RateResponse{
 		Rate: rate,
 	}, nil
+}
+
+func (s CurrencyServer) SubscribeRates(src currency.Currency_SubscribeRatesServer) error {
+	go func() {
+		for {
+			rr, err := src.Recv()
+			if err == io.EOF {
+				fmt.Println("client has closed connection")
+				break
+			}
+			if err != nil {
+				fmt.Println("unable to read from client", err)
+				break
+			}
+			fmt.Println("Handle client request", "request.base", rr.Base, "request.destionation", rr.Destination)
+		}
+	}()
+	for {
+		err := src.Send(&currency.RateResponse{Rate: 12.1})
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
